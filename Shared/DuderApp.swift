d1b2c5671c1c484899853d811c755577471bdf2a
josephlkremer <josephlkremer@gmail.com>
2021-06-06 21:13:08 -0600

    Initial Commit

---
+++ Shared/DuderApp.swift
@@ -0,0 +1,17 @@
+//
+//  DuderApp.swift
+//  Shared
+//
+//  Created by Joseph Kremer on 6/6/21.
+//
+
+import SwiftUI
+
+@main
+struct DuderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
